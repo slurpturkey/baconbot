ce6feebf25766d9c1fa6afb302ba62b01488c48f
slurpturkey <40962452+slurpturkey@users.noreply.github.com>
2019-02-10 11:09:29 +0000

    Added re-added !woke command

--- commands/random/woke.js
+++ commands/random/woke.js
@@ -6,12 +6,37 @@ class Woke extends commando.Command {
             name: 'woke',
             group: 'random',
             memberName: 'woke',
-            description: 'Woke'
+            description: 'Wokeifies the previous message'
         });
     }
 
     async run(message, args){
-        message.channel.send("Hold on a fucking day or two, goddamn.");
+        
+        let mColl = await message.channel.fetchMessages({limit: 2});
+        let mArray = await mColl.array();
+        let re = / /g;
+        let str = "";
+        let person;
+
+        if(!mArray[1].author.bot){
+            if(mArray[1].content.includes(" ")){
+                str = await mArray[1].content.replace(re, " :clap: ");
+                str = ":clap: " + str + " :clap:";
+            }
+            else str = ":clap: " + mArray[1].content + " :clap:";
+        }
+        else str = ":clap: fuck :clap: you :clap:"; 
+
+        message.channel.send({embed: {
+            color: 0xd65d00,
+            author: {
+                name: mArray[1].member.nickname,
+                icon_url: mArray[1].author.avatarURL
+            },
+            description: str
+        }});
+
+        //message.channel.send("Hold on a fucking day or two, goddamn.");
         
     }
 }
