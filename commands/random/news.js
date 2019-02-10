8e01feff9abeaef69b8c6d4ed9d4301864280ddd
slurpturkey <40962452+slurpturkey@users.noreply.github.com>
2019-02-10 11:26:51 +0000

    Re-added !news command (despite not being able to get images)

--- commands/random/news.js
+++ commands/random/news.js
@@ -1,4 +1,5 @@
 const commando = require('discord.js-commando');
+const request = require('request');
 
 class News extends commando.Command {
     constructor(client){
@@ -6,14 +7,32 @@ class News extends commando.Command {
             name: 'news',
             group: 'random',
             memberName: 'news',
-            description: 'News'
+            description: 'So much breaking news your screen will crack (I\'m ugly now because reddit blocked me from getting the preview images)'
         });
     }
 
     async run(message, args){
-        message.channel.send("Impatient twat.");
         
-        
+        request.get("http://www.reddit.com/r/nottheonion.json?limit=100", {}, function(err, res, body){
+            if(err) throw err;
+            
+            let result = JSON.parse(body);
+            let index = Math.floor((Math.random() * 50) + 1);
+
+            console.log(message.guild.me.user.avatarURL);
+
+            message.channel.send({embed: {
+                color: 0x13c37a, // turquoise
+                title: "Breaking News!",
+                description: "[" + result.data.children[index].data.title + "]" + '(http://www.reddit.com' + result.data.children[index].data.permalink + ")",
+                footer: {
+                    icon_url: message.guild.me.user.avatarURL,
+                    text: message.guild.me.nickname
+                }
+            }});
+        });
+
+        //message.channel.send("Impatient twat.");
     }
 }
 
