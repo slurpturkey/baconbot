6b720acfc622e1af57b8d87bbf1ecdedb0b452c4
slurpturkey <40962452+slurpturkey@users.noreply.github.com>
2022-05-07 18:29:00 +0100

    Add error checking to parse() function

--- commands/dictionary/wotd.js
+++ commands/dictionary/wotd.js
@@ -30,11 +30,31 @@ class WordOfTheDay extends commando.Command {
 
 function parse(body, message){
     let word = body.word;
-    let part = body.definitions[0].partOfSpeech;
-    let def = body.definitions[0].text;
-    let eg = body.examples[0].text;
-    let origin = body.note;
-
+    let part;
+    let def;
+    let eg;
+    let origin;
+    if(body.word)
+        word = body.word;
+    else 
+        word = "There is no word of the day today.";
+    if(body.examples)
+        eg = body.examples[0].text;
+    else
+        eg = "No example provided.";
+    if(body.note)
+        origin = body.note;
+    else
+        origin = "No origin provided.";
+    if(body.definitions[0].partOfSpeech)
+        part = body.definitions[0].partOfSpeech;
+    else
+        part = "No part of speech provided.";
+    if(body.definitions[0].text)
+        def = body.definitions[0].text;
+    else
+        def = "No definition provided.";
+    
     write(word, part, def, eg, origin, message);
 }
 
