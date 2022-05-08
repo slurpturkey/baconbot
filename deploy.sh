5eec66a99c542f2669f08901b76760bef87c2765
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-05-08 13:31:14 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,9 @@ readlink -f index.js
 
 # Copy service file, incase if there are any changes
 sudo cp baconbot.service /etc/systemd/system/baconbot.service
+
+sudo chmod -R 777 *
+
 # reload configurations incase if service file has changed
 sudo systemctl daemon-reload
 # restart the service
