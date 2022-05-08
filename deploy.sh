82feda607a378308e1f2956dd6f7de5a84f691ef
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-05-08 11:03:21 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,18 @@ sudo npm install
 #Change permissions of directory and files
 sudo chmod -R 777 *
 
-#Reload configuration
-sudo systemctl daemon-reload
+
 
 #Restart baconbot
 #sudo systemctl restart baconbot.service
-#sudo systemctl restart baconbot 
+# 
 sudo cp baconbot.service /etc/systemd/system/baconbot.service
 sudo systemctl enable baconbot.service
+
+#Reload configuration
+sudo systemctl daemon-reload
+
+sudo systemctl restart baconbot.service
 systemctl list-units --type=service
 systemctl list-unit-files
 #test
