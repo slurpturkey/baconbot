dc6691e2a4db99badbff3442c5f3c89976c1e0c3
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-05-08 14:33:50 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,6 @@ sudo npm install
 #Change permissions of directory and files
 sudo chmod -R 777 *
 
-readlink -f index.js
-
 # Copy service file, incase if there are any changes
 sudo cp baconbot.service /etc/systemd/system/baconbot.service
 
