396078c895fad52a43ab0bccbba607fa2fa128e7
tom1527 <70600248+tom1527@users.noreply.github.com>
2022-05-12 11:53:28 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-#Update dependencies
-cd /usr/local/bin/baconbot
-sudo npm install
-
 # Copy service file, incase if there are any changes
 sudo cp baconbot.service /etc/systemd/system/baconbot.service
-
-#Change permissions of directory and files
-sudo chmod -R 777 /usr/local/bin/baconbot
 
 # reload configurations incase if service file has changed
 sudo systemctl daemon-reload
