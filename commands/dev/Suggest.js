a7da5f7b5984c65acad6720ed5f95bae0b5e350c
tom <twbradley@hotmail.com>
2022-05-14 12:15:08 +0100

    Major refactor of suggestFeature

--- commands/dev/Suggest.js
+++ commands/dev/Suggest.js
@@ -9,9 +9,8 @@ const filePath = "vendorauth.json";
 const auth = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 const key = auth.trello_key;
 const token = auth.trello_token;
-const URL1 = "api.trello.com";
-const path = "/1/cards";
-const post_request_args = "?key=" + key + "&token=" + token;
+const baseUrl = "api.trello.com";
+const authParams = "?key=" + key + "&token=" + token;
 
 class Suggest extends commando.Command {
     constructor(client){
@@ -47,7 +46,6 @@ class Suggest extends commando.Command {
 
                     }
                     default: 
-                        console.log("defaulting");
                         message.channel.send(`Error: unrecognised flag: " - ${argsList[i].split(' ')[0]}"`);
                         return;
                 }
@@ -55,34 +53,83 @@ class Suggest extends commando.Command {
             const response = await GetLists.prototype.getListsRequest(message, args);
             var listNames = [];
             let bestMatch = null;
+            let bestMatchId = null
             let bestDistance = 9999;
             for(let i = 0; i < response.length; i++) {
                 listNames.push(response[i].name);
                 const distance = this.levenshteinDistance(list.toLowerCase().trim(), response[i].name.toLowerCase().trim());
                 if (distance < bestDistance) {
                     bestDistance = distance;
-                    console.log(bestDistance);
                     bestMatch = response[i].name;
+                    bestMatchId = response[i].id
                 }
             }
             if(bestDistance < 3) {
-                console.log(list);
-                console.log(bestMatch);
                 if(title.length) {
+                    let filter = m => m.author.id === message.author.id
                     message.channel.send({embed: {
                         color: 0x850000, // red
                         //Configure this to just use suggest || decide whether a seperate dev command is needed
                         description: `So you'd like to suggest the following card: \nTitle: ${title}\nDescription: ${desc ? desc : ""}\nOn list: ${bestMatch}`
-                    }});
+                    }}).then(() => {
+                    message.channel.awaitMessages(filter, {
+                            max: 1,
+                            time: 30000,
+                            errors: ['time']
+                        }).then(message => {
+                            message = message.first()
+                            if (message.content.toLowerCase() == 'yes' || message.content.toLowerCase() == 'y') {
+                                const data = JSON.stringify({
+                                    "name": title,
+                                    "desc": desc,
+                                    "pos": "top",
+                                    "idList": bestMatchId
+                                });
+                                const options = {
+                                    hostname: baseUrl,
+                                    method: 'POST',
+                                    path: `/1/cards${authParams}`,
+                                    headers: {
+                                        "Content-Type": "application/json",
+                                        "Content-Length": data.length,
+                                        key: key,
+                                        token: token
+                                    }
+                                };
+                                const req = https.request(options, res => {
+                                    console.log(`statusCode: ${res.statusCode}`)
+                                    
+                                    res.on('data', d => {
+                                        message.channel.send(`Suggestion sent`);
+                                        process.stdout.write(d);
+                                      });
+                                });
+                        
+                                req.on('error', error => {
+                                    message.channel.send(`Error: failed to send suggestion`);
+                                    console.error(error);
+                                });
+                        
+                                req.write(data);
+                                req.end();
+                                
+                            } else if (message.content.toLowerCase() == 'no' || message.content.toLowerCase() == 'n') {
+                                message.channel.send(`Cancelled`)
+                            } else {
+                                message.channel.send(`Error: invalid response`)
+                            }
+                        }).catch(collected => {
+                            message.channel.send('Timeout');
+                        });
+                    })
                 } else {
-                    message.channel.send("Title required. Please enter title after the <-title> flag.");
-                }
+                    message.channel.send("Title required. Please enter title after the <-title> flag.")}
             } else {
                 message.channel.send("Listname not recognised, please check for typos.");
             }
 
         } else {
-            
+            message.channel.send("Error: this command requires a title flag I.E \"!suggest -title <title>\"");
         }
     }
 
@@ -101,7 +148,6 @@ class Suggest extends commando.Command {
                 idList: "627859bfb5b5161121f4250b"
             });
 
-            console.log(options);
             const req = https.request(options, res => {
                 console.log(`statusCode: ${res.statusCode}`)
                 
